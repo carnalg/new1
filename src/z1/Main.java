857ee3db600f5e4331c30f8360d84bf0be2247fc
Yury Korsun <Zmey_85@bk.ru>
2018-06-08 19:59:09 +0300

    version1

---
+++ src/z1/Main.java
@@ -0,0 +1,43 @@
+package z1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=null;
+        int z;
+        try {
+            scanner = new Scanner(new File("src/in.txt"));
+
+            int[] numbers = new int[11];
+
+           for(int i=0;i<numbers.length;i++){
+               numbers[i]=scanner.nextInt();
+           }
+
+           for(int i=0;i<numbers.length;i++){
+               for(int j=0;j<numbers.length-1;j++){
+                   if(numbers[j]>numbers[j+1]){
+                       z=numbers[j];
+                       numbers[j]=numbers[j+1];
+                       numbers[j+1]=z;
+                   }
+               }
+           }
+
+
+            for(int i=0;i<numbers.length;i++) {
+                System.out.println(numbers[i]);
+            }
+
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
